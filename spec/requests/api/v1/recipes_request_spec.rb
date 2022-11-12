886dde110c40c648f42b5c1bb60f8cb19b0a3f8c
Thomas Musselman <tmusselman@gmail.com>
2022-11-12 16:57:56 -0700

    test: get recipes endpoint with country provided

--- spec/requests/api/v1/recipes_request_spec.rb
+++ spec/requests/api/v1/recipes_request_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Recipes Requests' do
-  
+  it 'returns a list of recipes when the country is provided', vcr: {cassette_name: 'greek recipes'} do
+    get '/api/v1/recipes?country=greece'
+
+    expect(response).to be_successful
+    expect(response).to have_http_status(200)
+
+    body = JSON.parse(response.body, symbolize_names: true)
+
+    expect(body).to have_key(:data)
+    expect(body[:data]).to be_an Array
+    
+    body[:data].each do |recipe|
+      expect(recipe[:id]).to be(nil)
+      expect(recipe[:type]).to eq("recipe")
+      expect(recipe[:attributes]).to be_a(Hash)
+      expect(recipe[:attributes][:url]).to be_a(String)
+      expect(recipe[:attributes][:title]).to be_a(String)
+      expect(recipe[:attributes][:image]).to be_a(String)
+      expect(recipe[:attributes][:country]).to eq("greece")
+    end
+  end
 end
