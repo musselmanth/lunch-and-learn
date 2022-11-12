f6934239623b57f5b082a97e7247b8d0fc45c5c2
Thomas Musselman <tmusselman@gmail.com>
2022-11-12 15:22:08 -0700

    test: recipes service with country provided

---
+++ spec/services/recipes_service_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe RecipesService do
+  it 'returns recipes for a country', :vcr do
+    recipes = RecipesService.get_recipes("greece")
+    
+    expect(recipes).to be_a(Hash)
+    expect(recipes).to have_key(:hits)
+    expect(recipes[:hits]).to be_an(Array)
+
+    first_recipe = recipes[:hits].first
+    expect(first_recipe).to be_a(Hash)
+    expect(first_recipe).to have_key(:recipe)
+    expect(first_recipe[:recipe]).to have_key(:label)
+    expect(first_recipe[:recipe][:label]).to be_a(String)
+    expect(first_recipe[:recipe]).to have_key(:image)
+    expect(first_recipe[:recipe][:image]).to be_a(String)
+    expect(first_recipe[:recipe]).to have_key(:url)
+    expect(first_recipe[:recipe][:url]).to be_a(String)
+  end
+end
