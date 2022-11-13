6039b84bf7e857fbd8b01dbe6ef6bfdb5edd1d99
Thomas Musselman <tmusselman@gmail.com>
2022-11-12 22:33:36 -0700

    fix typo in test

--- spec/facades/countries_facade_spec.rb
+++ spec/facades/countries_facade_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe CountriesFacade do
   it 'returns a random country name', vcr: {cassette_name: 'all countries'} do
     country = CountriesFacade.random_country
-    expect(country).to be_a(String)qexq
+    expect(country).to be_a(String)
   end
 end
