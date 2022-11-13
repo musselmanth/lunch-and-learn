0749dc590c1075178ba515bb1161f3491c1664bc
Thomas Musselman <tmusselman@gmail.com>
2022-11-12 21:36:19 -0700

    feat: implement learning resource facade

---
+++ app/facades/learning_resources_facade.rb
@@ -0,0 +1,7 @@
+class LearningResourcesFacade
+  def self.by_country(country)
+    video_data = YoutubeService.get_mr_history_videos(country)[:items].first
+    images_data = UnsplashService.get_image_search(country)[:results]
+    LearningResource.new(video_data, images_data, country)
+  end
+end
