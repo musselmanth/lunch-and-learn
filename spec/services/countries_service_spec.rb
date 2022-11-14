93631d06e7422b76ca30429f9094efcd2ff1f9a4
Thomas Musselman <tmusselman@gmail.com>
2022-11-14 09:23:18 -0700

    test for country service to get one country

--- spec/services/countries_service_spec.rb
+++ spec/services/countries_service_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe CountriesService do
   it 'returns a list of all countries', vcr: {cassette_name: 'all countries'} do
     countries = CountriesService.get_all_countries
+    
     expect(countries).to be_an(Array)
     expect(countries.length).to eq(250)
     countries.each do |country|
@@ -12,5 +13,20 @@ RSpec.describe CountriesService do
       expect(country[:name]).to have_key(:common)
       expect(country[:name][:common]).to be_a String
     end
+  end
+
+  it 'returns a single country by name', vcr: {cassette_name: 'france country info'} do
+    country = CountriesService.get_country("france")
+    
+    expect(country).to be_an Array
+
+    country = country.first
+
+    expect(country).to be_a Hash
+    expect(country[:name][:common]).to eq("France")
+    expect(country[:capitalInfo][:latlng]).to be_an Array
+    expect(country[:capitalInfo][:latlng].length).to eq(2)
+    expect(country[:capitalInfo][:latlng][0]).to be_a Float
+    expect(country[:capitalInfo][:latlng][1]).to be_a Float
   end
 end
