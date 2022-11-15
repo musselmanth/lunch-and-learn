134513f4aff52ed3ecf8fea7ac4fac21d554ac7a
Thomas Musselman <tmusselman@gmail.com>
2022-11-14 22:14:11 -0700

    add timestamps

--- db/migrate/20221115033226_create_favorites.rb
+++ db/migrate/20221115033226_create_favorites.rb
@@ -5,6 +5,7 @@ class CreateFavorites < ActiveRecord::Migration[5.2]
       t.string :recipe_link
       t.string :recipe_title
       t.references :user, foreign_key: true
+      t.timestamps
     end
   end
 end
