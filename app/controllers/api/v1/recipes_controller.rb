173e09a83bf123cd8eb77aa2e1a2fdf69ae4c6e9
Thomas Musselman <tmusselman@gmail.com>
2022-11-15 08:41:26 -0700

    refactor recipes controller to validate country

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::RecipesController < ApplicationController
   def index
     country = params[:country] || CountriesFacade.random_country
-    recipes = RecipesFacade.by_country(country)
-    render json: RecipeSerializer.new(recipes)
+    if CountriesFacade.valid_country?(country)
+      recipes = RecipesFacade.by_country(country)
+      render json: RecipeSerializer.new(recipes)
+    else
+      render json: ErrorSerializer.invalid_country, status: :not_found
+    end
   end
+
+
 end
