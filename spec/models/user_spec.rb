6409832599aa12c80dba0fb02755c55eac15ceda
Thomas Musselman <tmusselman@gmail.com>
2022-11-14 20:58:41 -0700

    update shoulda matchers for have_secure_token matcher

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it {should validate_presence_of :name}
   it {should validate_presence_of :email}
-  it {should validate_presence_of :api_key}
   it {should validate_uniqueness_of :email}
-  it {should validate_uniqueness_of :api_key}
   it {should have_secure_password}
+  it {should have_secure_token :api_key}
   it {should have_many :favorites}
 end
